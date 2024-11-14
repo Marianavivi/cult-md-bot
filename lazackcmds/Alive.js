29ff145d83561e49a0c4f859388f1893a5e02c25
Mariana <marianagutierrezvivi@gmail.com>
2024-11-14 18:03:54 +0300

    Update Alive.js

--- lazackcmds/Alive.js
+++ lazackcmds/Alive.js
@@ -1,14 +1,18 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  // Sound
-  let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/SilvaTechB/silva-md-bot@main/media/Alive.mp3'
-  let url = 'https://github.com/SilvaTechB/silva-md-bot'
-  let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
-  let img = 'https://files.catbox.moe/8324jm.jpg'
+  // Retrieve user details
+  let name = m.pushName || conn.getName(m.sender);
+  
+  // URLs and media
+  var vn = 'https://cdn.jsdelivr.net/gh/Marianavivi/cult-md-bot@main/media/Alive.mp3';
+  let url = 'https://github.com/Marianavivi/cult-md-bot';
+  let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e';
+  let img = 'https://files.catbox.moe/y1hq7c.jpg';
+
+  // Create a contact message object
   let con = {
     key: {
       fromMe: false,
-      participant: `${m.sender.split`@`[0]}@s.whatsapp.net`,
+      participant: `${m.sender.split('@')[0]}@s.whatsapp.net`,
       ...(m.chat ? { remoteJid: '254700143167@s.whatsapp.net' } : {}),
     },
     message: {
@@ -17,7 +21,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
-  }
+  };
+
+  // Prepare the audio message with context info
   let doc = {
     audio: {
       url: vn,
@@ -30,21 +36,23 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '𝐒𝐈𝐋𝐕𝐀 𝐌𝐃 𝐁𝐎𝐓 Alive',
-        body: 'SILVA MD BOT OCTOBER 2024',
+        title: 'CULT IS ALIVE',
+        body: 'WANNA JOIN CULT',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v',
+        sourceUrl: 'https://whatsapp.com/channel/0029Varf29L8PgsDJuAJtn0G',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
     },
-  }
+  };
 
-  await conn.sendMessage(m.chat, doc, { quoted: con })
-}
+  // Send the prepared audio message
+  await conn.sendMessage(m.chat, doc, { quoted: con });
+};
 
-handler.help = ['alive']
-handler.tags = ['main']
-handler.command = /^(alive)$/i
+// Command metadata
+handler.help = ['alive'];
+handler.tags = ['main'];
+handler.command = /^(alive)$/i;
 
-export default handler
+export default handler;
